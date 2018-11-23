c36ec3578aef7224082984ba5d282f917b2f2617
Acshi Haggenmiller <acshikh@umich.edu>
2018-11-23 14:46:33 -0500

    added competition bike model

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use libc::c_void;
 use std::ptr;
 use std::fs::File;
 use std::io::Read;
+use std::f64;
 
 extern crate lapack;
 extern crate blas;
@@ -377,6 +378,7 @@ Simplify(XPart)
 Simplify(UPart)
 (h^4*B1*A2^2*A3+2*h^3*B1*A2^2+2*h^3*A2*B2*A3+4*h^2*B1*A2+4*h^2*A2*B2+4*h^2*B2*A3+4*h*B1+16*h*B2+4*h*B3)/24
 */
+#[allow(dead_code)]
 fn rk4_constraints<F, G>(n: usize, m: usize, idx: usize, horizon: usize, dt: f64,
                          a_fun: &F, b_fun: &G,
                          aeq_row_idxs: &mut [i32], aeq_col_idxs: &mut [i32],
@@ -526,6 +528,7 @@ Simplify(XPart)
 Simplify(UPart)
 (h^2*A2*B1 + 2*h*B2)/2
 */
+#[allow(dead_code)]
 fn rk2_constraints<F, G>(n: usize, m: usize, idx: usize, horizon: usize, dt: f64,
                          a_fun: &F, b_fun: &G,
                          aeq_row_idxs: &mut [i32], aeq_col_idxs: &mut [i32],
@@ -610,6 +613,7 @@ where F: Fn(f64, &mut [f64]),
     }
 }
 
+#[allow(dead_code)]
 fn euler_constraints<F, G>(n: usize, m: usize, idx: usize, horizon: usize, dt: f64,
                            a_fun: &F, b_fun: &G,
                            aeq_row_idxs: &mut [i32], aeq_col_idxs: &mut [i32],
@@ -938,57 +942,74 @@ where F: FnMut(f64, &[f64], &mut [f64]) {
     }
 }
 
-// fn forward_integrate_control_inputs(t: f64, x: &[f64], time_steps: &[f64], controls: &[f64], dx_dt: &mut [f64]) {
-//     let Nw = 2.;
-//     let f = 0.01;
-//     let Iz = 2667.0;
-//     let a = 1.35;
-//     let b = 1.45;
-//     let By = 0.27;
-//     let Cy = 1.2;
-//     let Dy = 0.7;
-//     let Ey = -1.6;
-//     let Shy = 0.0;
-//     let Svy = 0.0;
-//     let m = 1400.0;
-//     let g = 9.806;
-//
-//     // %generate input functions
-//     // delta_f=interp1(T,U(:,1),t,'previous','extrap');
-//     // F_x=interp1(T,U(:,2),t,'previous','extrap');
-//
-//     // slip angle functions in degrees
-//     let a_f = rad2deg(delta_f-atan2(x[3]+a*x[5], x[1]));
-//     let a_r = rad2deg(-atan2((x[3]-b*x[5]),x[1]));
-//
-//     %Nonlinear Tire Dynamics
-//     phi_yf=(1-Ey)*(a_f+Shy)+(Ey/By)*atan(By*(a_f+Shy));
-//     phi_yr=(1-Ey)*(a_r+Shy)+(Ey/By)*atan(By*(a_r+Shy));
-//
-//     F_zf=b/(a+b)*m*g;
-//     F_yf=F_zf*Dy*sin(Cy*atan(By*phi_yf))+Svy;
-//
-//     F_zr=a/(a+b)*m*g;
-//     F_yr=F_zr*Dy*sin(Cy*atan(By*phi_yr))+Svy;
-//
-//     F_total=sqrt((Nw*F_x)^2+(F_yr^2));
-//     F_max=0.7*m*g;
-//
-//     if F_total>F_max
-//
-//         F_x=F_max/F_total*F_x;
-//
-//         F_yr=F_max/F_total*F_yr;
-//     end
-//
-//     %vehicle dynamics
-//     dzdt= [x(2)*cos(x(5))-x(4)*sin(x(5));...
-//               (-f*m*g+Nw*F_x-F_yf*sin(delta_f))/m+x(4)*x(6);...
-//               x(2)*sin(x(5))+x(4)*cos(x(5));...
-//               (F_yf*cos(delta_f)+F_yr)/m-x(2)*x(6);...
-//               x(6);...
-//               (F_yf*a*cos(delta_f)-F_yr*b)/Iz];
-// }
+fn forward_integrate_bike(controls: &[f64]) -> Vec<f64> {
+    let n = 6;
+    let m = 2;
+    let x0 = [287.0, 5.0, -176.0, 0.0, 2.0, 0.0];
+    let dt = 0.01;
+    let rk4_steps = 10;
+    let all_steps = (controls.len() / m - 1) * rk4_steps;
+    let mut x_all = vec![0.0; n * all_steps + n];
+
+    let mut ode_fun = |t: f64, x: &[f64], dx: &mut [f64]| {
+        let idx = (t / 0.01 + 0.0001) as usize;
+        let idx = if idx == controls.len() / m { idx - 1 } else { idx };
+        bike_fun(x, controls[idx * m], controls[idx * m + 1], dx);
+    };
+    rk4_integrate(dt / rk4_steps as f64, all_steps, &mut ode_fun, &x0, &mut x_all);
+
+    x_all
+}
+
+fn rad2deg(r: f64) -> f64 {
+    return r * 180.0 / f64::consts::PI;
+}
+
+fn bike_fun(x: &[f64], delta_f: f64, mut f_x: f64, dx: &mut [f64]) {
+    let nw = 2.;
+    let f = 0.01;
+    let iz = 2667.0;
+    let a = 1.35;
+    let b = 1.45;
+    let by = 0.27;
+    let cy = 1.2;
+    let dy = 0.7;
+    let ey = -1.6;
+    let shy = 0.0;
+    let svy = 0.0;
+    let m = 1400.0;
+    let g = 9.806;
+
+    // slip angle functions in degrees
+    let a_f = rad2deg(delta_f - (x[3]+a*x[5]).atan2(x[1]));
+    let a_r = rad2deg(-(x[3]-b*x[5]).atan2(x[1]));
+
+    // Nonlinear Tire dynamics
+    let phi_yf = (1.0-ey)*(a_f+shy)+(ey/by)*(by*(a_f+shy)).atan();
+    let phi_yr = (1.0-ey)*(a_r+shy)+(ey/by)*(by*(a_r+shy)).atan();
+
+    let f_zf=b/(a+b)*m*g;
+    let f_yf=f_zf*dy*(cy*(by*phi_yf).atan()).sin()+svy;
+
+    let f_zr=a/(a+b)*m*g;
+    let mut f_yr=f_zr*dy*(cy*(by*phi_yr).atan()).sin()+svy;
+
+    let f_total=((nw*f_x).powi(2)+f_yr*f_yr).sqrt();
+    let f_max=0.7*m*g;
+
+    if f_total > f_max {
+        f_x=f_max/f_total*f_x;
+        f_yr=f_max/f_total*f_yr;
+    }
+
+    // vehicle dynamics
+    dx[0] = x[1]*x[4].cos()-x[3]*x[4].sin();
+    dx[1] = (-f*m*g+nw*f_x-f_yf*delta_f.sin())/m+x[3]*x[5];
+    dx[2] = x[1]*x[4].sin()+x[3]*x[4].cos();
+    dx[3] = (f_yf*delta_f.cos()+f_yr)/m-x[1]*x[5];
+    dx[4] = x[5];
+    dx[5] = (f_yf*a*delta_f.cos()-f_yr*b)/iz;
+}
 
 fn quadprog_test() {
     let h_mat = [1., -1., 1., -1., 2., -2., 1., -2., 4.];
@@ -1280,6 +1301,19 @@ fn mpc_test() {
     }
 }
 
+fn bike_test() {
+    let controls = vec![0.0; 20];
+    let x_all = forward_integrate_bike(&controls);
+    let n = 6;
+    let n_steps = x_all.len() / n;
+    for i in (0..n_steps).step_by(10) {
+        for j in 0..n {
+            print!("{:9.4}  ", x_all[i * n + j]);
+        }
+        println!("");
+    }
+}
+
 fn main() {
     if false {
         quadprog_test();
@@ -1289,5 +1323,8 @@ fn main() {
     if false {
         lqr_test();
     }
-    mpc_test();
+    if false {
+        mpc_test();
+    }
+    bike_test();
 }
