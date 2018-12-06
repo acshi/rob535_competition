45e675d8ec04ccf24c324c78de03cd8e2e77f797
Acshi Haggenmiller <acshikh@umich.edu>
2018-12-06 16:15:56 -0500

    submission1 in

--- test_matlab_run.m
+++ test_matlab_run.m
@@ -1,8 +1,9 @@
 addpath('./SimPkg_F18_V1');
+addpath('./rob535submission1');
 
 load 'TestTrack.mat';
-% Xobs = generateRandomObstacles(10);
-% U = ROB599_ControlProject_part2_Team5(TestTrack, Xobs);
+Xobs = generateRandomObstacles(10);
+U = ROB599_ControlsProject_part2_Team5(TestTrack, Xobs);
 x0 = [287; 5; -176; 0; 2; 0];
 [Y, ~] = forwardIntegrateControlInput(U, x0);
 info = getTrajectoryInfo(Y, U, Xobs, TestTrack);
