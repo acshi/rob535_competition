1ae6985b95e720afd1f8a0a85534958850ff49c6
Max Krogius <mkrogius@umich.edu>
2018-12-14 18:03:49 -0500

    RK4 derivatives

--- matlab/rk4_cons.m
+++ matlab/rk4_cons.m
@@ -8,36 +8,11 @@ for i=1:(nSteps-1)
     x = X(8*i-7:8*i-2);
     u = X(8*i-1:8*i);
     u(2) = Fx_scale*u(2);
-    x1 = rk4_integrate(x, u, dt, 4);
+    [x1, dx0, du] = rk4_integrate(x, u, dt, 4);
     C(6*i-5:6*i) = X(8*i+1:8*i+6) - x1;
-    
-    k1 = nonl_bike([x; u]);
-    k2 = nonl_bike([x + 0.5*dt*k1; u]);
-    k3 = nonl_bike([x + 0.5*dt*k2; u]);
-    k4 = nonl_bike([x + dt*k3; u]);
-    x1 = x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4);
-    
     dC(6*i-5:6*i, 8*i+1:8*i+6) = eye(6);
-    [A1, B1] = getDerivatives(x, u);
-    [A2, B2] = getDerivatives(x + 0.5*dt*k1, u);
-    [A3, B3] = getDerivatives(x + 0.5*dt*k2, u);
-    [A4, B4] = getDerivatives(x + dt*k3, u);
-    B1(:,2) = Fx_scale*B1(:,2);
-    B2(:,2) = Fx_scale*B2(:,2);
-    B3(:,2) = Fx_scale*B3(:,2);
-    B4(:,2) = Fx_scale*B4(:,2);
-    
-    dk1dx = A1;
-    dk2dx = A2*(eye(6) + 0.5*dt*dk1dx);
-    dk3dx = A3*(eye(6) + 0.5*dt*dk2dx);
-    dk4dx = A4*(eye(6) + dt*dk3dx);
-
-    dC(6*i-5:6*i, 8*i-7:8*i-2) = -(eye(6) + (dt/6)*(dk1dx + 2*dk2dx + 2*dk3dx + dk4dx));
-    
-    dk1du = B1;
-    dk2du = (dt/2)*A2*dk1du + B2;
-    dk3du = (dt/2)*A3*dk2du + B3;
-    dk4du = dt*A4*dk3du + B4;
-    dC(6*i-5:6*i, 8*i-1:8*i) = -(dt/6)*(dk1du + 2*dk2du + 2*dk3du + dk4du);
+    du(:,2) = Fx_scale*du(:,2);
+    dC(6*i-5:6*i, 8*i-7:8*i-2) = -dx0;
+    dC(6*i-5:6*i, 8*i-1:8*i) = -du;
 end
 end
--- matlab/rk4_integrate.m
+++ matlab/rk4_integrate.m
@@ -1,6 +1,8 @@
-function [x1] = rk4_integrate(x0, u, dt, nSteps)
+function [x1, dx0, du] = rk4_integrate(x0, u, dt, nSteps)
 x1 = x0;
 h = dt/nSteps;
+dx0 = eye(6);
+du = zeros(6,2);
 for i=1:nSteps
     x = x1;
     k1 = nonl_bike([x; u]);
@@ -8,5 +10,25 @@ for i=1:nSteps
     k3 = nonl_bike([x + 0.5*h*k2; u]);
     k4 = nonl_bike([x + h*k3; u]);
     x1 = x + (h/6)*(k1 + 2*k2 + 2*k3 + k4);
+
+    [A1, B1] = getDerivatives(x, u);
+    [A2, B2] = getDerivatives(x + 0.5*h*k1, u);
+    [A3, B3] = getDerivatives(x + 0.5*h*k2, u);
+    [A4, B4] = getDerivatives(x + h*k3, u);
+    
+    dk1dx = A1;
+    dk2dx = A2*(eye(6) + 0.5*h*dk1dx);
+    dk3dx = A3*(eye(6) + 0.5*h*dk2dx);
+    dk4dx = A4*(eye(6) + h*dk3dx);
+
+    dx1dx = eye(6) + (h/6)*(dk1dx + 2*dk2dx + 2*dk3dx + dk4dx);
+    dx0 = dx1dx*dx0;
+    
+    dk1du = B1;
+    dk2du = (h/2)*A2*dk1du + B2;
+    dk3du = (h/2)*A3*dk2du + B3;
+    dk4du = h*A4*dk3du + B4;
+    dx1du = (h/6)*(dk1du + 2*dk2du + 2*dk3du + dk4du);
+    du = dx1dx*du + dx1du;
 end
 end
