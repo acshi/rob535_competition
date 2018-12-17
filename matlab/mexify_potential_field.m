7a3eea830c36189eb16958a8df8911befc1e66f2
Acshi Haggenmiller <acshikh@umich.edu>
2018-12-17 14:00:55 -0500

    use release builds in matlab

--- matlab/mexify_potential_field.m
+++ matlab/mexify_potential_field.m
@@ -1,2 +1,2 @@
-mex -g -ldl potential_field.c ../lib-potential-field/target/debug/libpotential_field.a
-%mex -g potential_field.c ../lib-potential-field/target/debug/potential_field.lib WS2_32.Lib Userenv.lib
+mex -g -ldl potential_field.c ../lib-potential-field/target/release/libpotential_field.a
+% mex -g potential_field.c ../lib-potential-field/target/release/potential_field.lib WS2_32.Lib Userenv.lib
